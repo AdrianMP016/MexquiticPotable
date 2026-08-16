daca94363de4a7ae1059a850dc4c13432d8ccb4d
AdrianMP016 <adrianmp016@users.noreply.github.com>
2026-08-16 11:17:30 -0600

    Delete _diag_exec_test.php

--- _diag_exec_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: application/json; charset=utf-8');
-
-echo json_encode([
-    'ziparchive_existe' => class_exists('ZipArchive'),
-    'zip_extension_loaded' => extension_loaded('zip'),
-    'php_version' => PHP_VERSION,
-], JSON_PRETTY_PRINT);
