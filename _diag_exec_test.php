a83978809b1de9102d4ad71cc8bbb111c6e617a6
AdrianMP016 <adrianmp016@users.noreply.github.com>
2026-08-16 11:14:25 -0600

    Update _diag_exec_test.php

--- _diag_exec_test.php
+++ _diag_exec_test.php
@@ -2,33 +2,30 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-$resultado = [
-    'exec_existe' => function_exists('exec'),
-    'shell_exec_existe' => function_exists('shell_exec'),
-    'disable_functions' => ini_get('disable_functions'),
-    'open_basedir' => ini_get('open_basedir'),
+$resultado = [];
+
+$candidatos = [
+    'python3', 'python',
+    '/usr/bin/python3', '/usr/local/bin/python3',
+    '/opt/alt/python311/bin/python3', '/opt/alt/python310/bin/python3',
+    '/opt/alt/python39/bin/python3', '/opt/alt/python38/bin/python3',
+    '/opt/cpanel/ea-python311/root/usr/bin/python3',
+    '/opt/cpanel/ea-python39/root/usr/bin/python3',
 ];
 
-if (function_exists('exec')) {
-    $out1 = [];
-    $code1 = 0;
-    @exec('python3 --version 2>&1', $out1, $code1);
-    $resultado['python3_version'] = ['salida' => $out1, 'codigo' => $code1];
-
-    $out2 = [];
-    $code2 = 0;
-    @exec('python --version 2>&1', $out2, $code2);
-    $resultado['python_version'] = ['salida' => $out2, 'codigo' => $code2];
-
-    $out3 = [];
-    $code3 = 0;
-    @exec('which python3 2>&1', $out3, $code3);
-    $resultado['which_python3'] = ['salida' => $out3, 'codigo' => $code3];
-
-    $out4 = [];
-    $code4 = 0;
-    @exec('python3 -c "import openpyxl; print(openpyxl.__version__)" 2>&1', $out4, $code4);
-    $resultado['openpyxl_version'] = ['salida' => $out4, 'codigo' => $code4];
+foreach ($candidatos as $candidato) {
+    $out = [];
+    $code = 0;
+    @exec(escapeshellarg($candidato) . ' --version 2>&1', $out, $code);
+    $resultado[$candidato] = ['salida' => $out, 'codigo' => $code];
 }
+
+$out = [];
+@exec('find /opt -maxdepth 3 -iname "python3*" -type f 2>/dev/null', $out);
+$resultado['busqueda_opt'] = $out;
+
+$out = [];
+@exec('ls /opt/alt 2>&1', $out);
+$resultado['ls_opt_alt'] = $out;
 
 echo json_encode($resultado, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
