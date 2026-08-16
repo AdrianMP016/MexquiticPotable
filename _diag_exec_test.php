32d5a1ed78eb853f2e71ca509d8bb9d1bf7108af
AdrianMP016 <adrianmp016@users.noreply.github.com>
2026-08-16 11:16:51 -0600

    Update _diag_exec_test.php

--- _diag_exec_test.php
+++ _diag_exec_test.php
@@ -2,22 +2,8 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-$python = '/opt/alt/python311/bin/python3';
-$resultado = [];
-
-$out = [];
-$code = 0;
-@exec(escapeshellarg($python) . ' -c "import openpyxl; print(openpyxl.__version__)" 2>&1', $out, $code);
-$resultado['openpyxl'] = ['salida' => $out, 'codigo' => $code];
-
-$out2 = [];
-$code2 = 0;
-@exec(escapeshellarg($python) . ' -m pip --version 2>&1', $out2, $code2);
-$resultado['pip'] = ['salida' => $out2, 'codigo' => $code2];
-
-$out3 = [];
-$code3 = 0;
-@exec(escapeshellarg($python) . ' -m pip list 2>&1', $out3, $code3);
-$resultado['pip_list'] = ['salida' => $out3, 'codigo' => $code3];
-
-echo json_encode($resultado, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+echo json_encode([
+    'ziparchive_existe' => class_exists('ZipArchive'),
+    'zip_extension_loaded' => extension_loaded('zip'),
+    'php_version' => PHP_VERSION,
+], JSON_PRETTY_PRINT);
